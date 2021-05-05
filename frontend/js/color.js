a7b65b17b34350f4eae0a7665aef4359ba659edf
Vyshnavi Puniparthi <vyshupuniparthi@gmail.com>
2021-05-05 21:10:39 +0530

    added colorpick

---
+++ frontend/js/color.js
@@ -0,0 +1,11 @@
+function changeColor() {
+    let Red= document.getElementById('rangeRed').value;
+    let Green= document.getElementById('rangeGreen').value;
+    let Blue= document.getElementById('rangeBlue').value;
+    let color = "RGB("+ Red + "," + Green + "," + Blue + ")";
+    document.body.style.backgroundColor = color;
+    document.getElementById('colorOutput').innerHTML = ':' + color;
+}
+document.getElementById('rangeRed').addEventListener('input',changeColor);
+document.getElementById('rangeGreen').addEventListener('input',changeColor);
+document.getElementById('rangeBlue').addEventListener('input',changeColor);

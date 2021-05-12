a891708a5e2fa168a8b2c2a8ceb2194074fa987f
Vyshnavi Puniparthi <vyshupuniparthi@gmail.com>
2021-05-13 00:47:56 +0530

    added todo

--- frontend/js/todo.js
+++ frontend/js/todo.js
@@ -5,9 +5,9 @@ let inputField = document.getElementById('inputField');
 addToDoButton.addEventListener('click', function(){
     var paragraph = document.createElement('p');
     paragraph.classList.add('paragraph-styling');
-    paragraph.innerText = inputField.nodeValue;
+    paragraph.innerText = inputField.value;
     toDoContainer.appendChild(paragraph);
-    inputField.nodeValue= "";
+    inputField.value= "";
     paragraph.addEventListener('click', function(){
         paragraph.style.textDecoration = "line-through";
     })
